c51fd218db3d3c2bb3e6ce1191dab2e3e2f25181
Ronald Diemicke <ronald.diemicke@gmail.com>
2025-01-19 19:49:02 -0600

    Updating to run unzip task in rust.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,4 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+mod unzip;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -15,7 +11,8 @@ pub fn run() {
         //        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_upload::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            unzip::unzip_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
